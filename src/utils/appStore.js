cd1b57c11a47389ba68701bf0ef85863256fd1c5
Saurabh Sushir <sushirss1@gmail.com>
2024-12-04 17:30:05 +0530

    Final commit

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,13 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./useSlice"
 import moviesReducer from "./moviesSlice"
+import gptReducer from "./gptSlice"
+import configReducer from "./configSlice"
 
 
 const appStore = configureStore({
     reducer:{
         user : userReducer,
         movies : moviesReducer,
-
+        gpt : gptReducer,
+        config: configReducer,
     },
 })
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -9,3 +9,18 @@ export const API_OPTIONS =  {
           Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ODUxMTEyYjlkOTNmZTA1Y2MyN2U3MmM2YmEyMzc2NCIsIm5iZiI6MTcyOTQyNjYxMy4yNzUyNzMsInN1YiI6IjY3MTRlYzEzNjUwMjQ4YjlkYjYyNzFmOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mqRT-qDehOVsOnGyWRfHI4icdFm8IfLgh790_C045iY'
         }
       };
+
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500/"
+
+export const BG_IMG_URL = "https://assets.nflxext.com/ffe/siteui/vlv3/4d2c5849-b306-4884-9036-6211f7ee0178/web/IN-en-20240930-TRIFECTA-perspective_1e1ca6cd-9e2d-4e9d-9e4b-ba0c2d3a0e31_small.jpg"
+
+
+export const SUPPORTED_LANGUAGES = [{identifier:"en",name:"English"},
+  {identifier:"hindi",name:"Hindi"},{identifier:"spanish",name:"Spanish"}
+]
+
+
+export const OPENAI_KEY = "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"
+  
+
